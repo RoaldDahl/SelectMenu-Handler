5e161700f1913342fe5aa65b3a9f4f8178c2d7da
RoaldDahl <95515434+RoaldDahl@users.noreply.github.com>
2022-09-02 20:06:54 +0930

    Update selectMenuListener.js

--- selectMenuListener.js
+++ selectMenuListener.js
@@ -13,7 +13,7 @@ module.exports = {
 
     if (selectMenu.permission && !interaction.member.permissions.has(selectMenu.permission)) return interaction.reply({ embeds: [ new EmbedBuilder().setDescription( `⛔ | You don't have the required permissions to use this.`).setColor("#f8312f") ], ephemeral: true });
   
-    if (selectMenu.developer && interaction.user.id !== "611115652471717906") return interaction.reply({ embeds: [ new EmbedBuilder().setDescription( `⛔ | This select menu is for developers only.`).setColor("#f8312f") ], ephemeral: true });
+    if (selectMenu.developer && interaction.user.id !== "CHANGEME") return interaction.reply({ embeds: [ new EmbedBuilder().setDescription( `⛔ | This select menu is for developers only.`).setColor("#f8312f") ], ephemeral: true });
 
     await selectMenu.execute(interaction, client);
   },
